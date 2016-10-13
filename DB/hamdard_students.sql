77e224d261cb5ff3b9142fb90b505429b5a928af
Taha <muhammadtahakhan222@gmail.com>
2016-10-13 10:50:48 +0500

    added setp form template and updated DB

--- DB/hamdard_students.sql
+++ DB/hamdard_students.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 06, 2016 at 04:29 PM
+-- Generation Time: Oct 11, 2016 at 02:24 PM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 7.0.5
 
@@ -19,6 +19,60 @@ SET time_zone = "+00:00";
 --
 -- Database: `hamdard_students`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `board_university`
+--
+
+CREATE TABLE `board_university` (
+  `id` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `slug` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `city`
+--
+
+CREATE TABLE `city` (
+  `city_id` int(11) NOT NULL,
+  `country_id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `slug` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `city`
+--
+
+INSERT INTO `city` (`city_id`, `country_id`, `name`, `slug`) VALUES
+(2, 12, 'karachi', 'karachi'),
+(3, 1, 'lahor', 'lahor');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `country`
+--
+
+CREATE TABLE `country` (
+  `country_id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `slug` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `country`
+--
+
+INSERT INTO `country` (`country_id`, `name`, `slug`) VALUES
+(1, 'pakistan', 'paktaisn'),
+(2, 'india', 'india'),
+(3, 'germany', 'germany');
 
 -- --------------------------------------------------------
 
@@ -42,6 +96,56 @@ INSERT INTO `migration` (`version`, `apply_time`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `nationality`
+--
+
+CREATE TABLE `nationality` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `slug` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `nationality`
+--
+
+INSERT INTO `nationality` (`id`, `name`, `slug`) VALUES
+(1, 'pakistani', 'Pakistani');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `qualification`
+--
+
+CREATE TABLE `qualification` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `slug` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `qualification`
+--
+
+INSERT INTO `qualification` (`id`, `name`, `slug`) VALUES
+(1, 'matric', 'matric');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `test_center`
+--
+
+CREATE TABLE `test_center` (
+  `id` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `location` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
@@ -58,14 +162,58 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `auth_key`, `password_hash`, `password_reset_token`, `email`, `status`, `created_at`, `updated_at`) VALUES
+(1, 'admin', 'AqatJrc42GPAApWTdW7XlWaovT0h4JNt', '$2y$13$.05KrJglGh9FJ47p1CH6HePvxz5rAyXP4x/4DE0gsA3340UNbiv6a', NULL, 'admin@yahoo.com', 10, 1476035812, 1476035812);
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `board_university`
+--
+ALTER TABLE `board_university`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `city`
+--
+ALTER TABLE `city`
+  ADD PRIMARY KEY (`city_id`),
+  ADD KEY `country_id` (`country_id`);
+
+--
+-- Indexes for table `country`
+--
+ALTER TABLE `country`
+  ADD PRIMARY KEY (`country_id`);
 
 --
 -- Indexes for table `migration`
 --
 ALTER TABLE `migration`
   ADD PRIMARY KEY (`version`);
+
+--
+-- Indexes for table `nationality`
+--
+ALTER TABLE `nationality`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `qualification`
+--
+ALTER TABLE `qualification`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `test_center`
+--
+ALTER TABLE `test_center`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `user`
@@ -81,10 +229,40 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `board_university`
+--
+ALTER TABLE `board_university`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `city`
+--
+ALTER TABLE `city`
+  MODIFY `city_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `country`
+--
+ALTER TABLE `country`
+  MODIFY `country_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `nationality`
+--
+ALTER TABLE `nationality`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `qualification`
+--
+ALTER TABLE `qualification`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `test_center`
+--
+ALTER TABLE `test_center`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
